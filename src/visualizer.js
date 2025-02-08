1b750088c80b06f30bcb5cabf4b9321a7549b870
egonzalez99 <112592570+egonzalez99@users.noreply.github.com>
2025-02-07 22:59:28 -0500

    first step for data fetch to visual ize
    tips: press go live on vs code to host the website correctly. needs to worked on later.

--- src/visualizer.js
+++ src/visualizer.js
@@ -1,13 +1,33 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-// Append an SVG dynamically to the body
-const svg = d3.select("body")
-    .append("svg")
-    .attr("width", 500)
-    .attr("height", 500);
 
-svg.append("circle")
-    .attr("cx", 250)   // Center X position
-    .attr("cy", 250)   // Center Y position
-    .attr("r", 100)    // Radius
-    .attr("fill", "blue");  // Fill color
+async function nyweatherdata(params) {
+
+     // loading the json data
+    const data = await d3.json("nyweather.json");
+
+    data.json(weatherdata).then(data => {
+
+        weatherdata() = {
+            temperature : data.main.temp,
+            humidity : data.main.humidity,
+            windSpeed : data.main.windSpeed,
+            weatherDescription : data.weather[1].description
+        }
+        
+    }) 
+    // Append an SVG dynamically to the body
+    const svg = d3.select("body")
+        .append("svg")
+        .attr("width", 500)
+        .attr("height", 500);
+
+    svg.append("circle")
+        .attr("cx", 250)   // Center X position
+        .attr("cy", 250)   // Center Y position
+        .attr("r", 100)    // Radius
+        .attr("fill", "blue");  // Fill color
+    
+}
+
+nyweatherdata();
--- src/weatherdata.js
+++ src/weatherdata.js
@@ -17,27 +17,6 @@ export function fetchNewYorkData() {
   
         // Return data needed for adding the marker
         return { temp: nyTemp, latitude: nyLatitude, longitude: nyLongitude };
-      })
+      } )
       .catch(error => console.error('Error fetching New York weather data:', error));
-  }
-  
-  // fetch weather data for Florida
-  export function fetchFloridaData() {
-    const apiKey = '35e137dcc022e717eb9aaefadfae9332'; 
-    const flUrl = `http://api.openweathermap.org/data/2.5/weather?id=4155751&units=metric&appid=${apiKey}`; // Florida city ID
-  
-    return fetch(flUrl)
-      .then(response => response.json())
-      .then(data => {
-        const flTemp = data.main.temp;
-        console.log('Florida Temperature:', flTemp);
-  
-        // coordinates for FL
-        const flLatitude = 27.994402;
-        const flLongitude = -81.760254;
-  
-        // return data for the marker
-        return { temp: flTemp, latitude: flLatitude, longitude: flLongitude };
-      })
-      .catch(error => console.error('Error fetching Florida weather data:', error));
   }
