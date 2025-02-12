75cc313d06d004fba37572045f810b9093f9fd32
egonzalez99 <112592570+egonzalez99@users.noreply.github.com>
2025-02-11 23:04:12 -0500

    set up json link
    data is visualize but i want to update the file (that could be done later. might be better to add that later on.). Right now just fix visual chart more.

--- src/visualizer.js
+++ src/visualizer.js
@@ -1,54 +1,70 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 
-async function nyweatherdata(params) {
-
-     // loading the json data
+// Define an async function to load the data and visualize it
+async function visualizeWeather() {
+    // Load JSON data using await
     const data = await d3.json("nyweather.json");
 
-    //extracting the weather data 
-    const weatherdata = {
-        temperature : data.main.temp,
-        humidity : data.main.humidity,
-        windSpeed : data.main.windSpeed,
-        weatherDescription : data.weather[0].description,
-    }
-    
-    //to be shown in html
-    d3.select("wweatherDescription")
-        .append("h1")
-        // .text(Weather: ${weatherData.weatherDescription});
+    // Extract the relevant weather data
+    const weatherData = {
+        temperature: data.main.temp,
+        windSpeed: data.wind.speed,
+        weatherDescription: data.weather[0].description,
+    };
 
-    // Append an SVG dynamically to the body
+    // display in the HTML
+    d3.select("#weather-description")
+        .append("h2")
+        .text(`Weather: ${weatherData.weatherDescription}`); //needs to be in ' ' to avoid syntax error. idk why
+
+    // SVG dimensions for the chart
     const width = 500;
     const height = 500;
+
+    // Create SVG container
     const svg = d3.select("body")
         .append("svg")
         .attr("width", width)
         .attr("height", height);
-        const marginTop = 20;
-        const marginRight = 0;
-        const marginBottom = 30;
-        const marginLeft = 40;
 
-    //windspeed visual: this is for the x-axis and y-axis
-    const x = d3.scaleBand()
-        .domain(["Temperature", "WindSpeed"])
-        .range([marginLeft, width - marginRight])
-        .padding(0, 1);
-    
-    const y = d3.scaleLinear()
-        .domain([0, d3.max(weatherdata.temperature, weatherdata.windSpeed)]).nice()
-        .range([height - marginBottom, marginTop]);
-    
-    //bars for temperature and wind speed
+    // set up scales for the bars based on data values
+    const xScale = d3.scaleBand()
+        .domain(["Temperature", "Wind Speed"]) // the bar names
+        .range([0, width])
+        .padding(0.2); 
+
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max([weatherData.temperature, weatherData.windSpeed])]) 
+        .range([height, 0]); // set 0 at the bottom
+
+    // add chart bars for temperature and wind speed
     svg.selectAll("rect")
-        .data[weatherdata.temperature, weatherdata.windSpeed]
+        .data([weatherData.temperature, weatherData.windSpeed]) // The data for the bars
         .enter()
         .append("rect")
-        .attr("x", (d, i) => xScale(["Temperature", "Wind Speed"][i]))//x position
-        .attr("y", d => yScale(d))//y position from data vals
-        
+        // x position and y position based on the data values
+        .attr("x", (d, i) => xScale(["Temperature", "Wind Speed"][i])) 
+        .attr("y", d => yScale(d)) 
+        // width and height of the bars
+        .attr("width", xScale.bandwidth())
+        .attr("height", d => height - yScale(d)) 
+        .attr("fill", "steelblue");
+
+    // add x-axis to chart data
+    svg.append("g")
+        .attr("transform", `translate(0,${height})`) // needs to be in ' ' to avoid syntax error. idk why
+        .call(d3.axisBottom(xScale));
+
+    // add y-axis to chart data
+    svg.append("g")
+        .call(d3.axisLeft(yScale));
+
+    // outputs the json but i want the json to be updated daily until last year. so i need to modify the json
+    console.log("Temperature" , weatherData.temperature);// current: decimal point is celcuius. updatet to fahrenheit  
+
+    console.log("wind speed" , weatherData.windSpeed);// current: decimal point is m/s. updatet to mph
 }
 
-nyweatherdata();
+// calling the function to be visualized
+visualizeWeather();
