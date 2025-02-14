9d8fea05b907d8ccac30c34cdaabdec83f0a39da
egonzalez99 <112592570+egonzalez99@users.noreply.github.com>
2025-02-14 14:39:24 -0500

    added basic web scrap script
    using javascript i use the google custom search json api to filter out a word and fetch the data. its estimated values and not exact. since it goes against google's TOS.

--- searchterm.js
+++ searchterm.js
@@ -1,1 +1,29 @@
-const searchAPI = 'https://developers.google.com/custom-search/v1/overview';
+const searchKey = 'AIzaSyBVoUsBH-9kjhNFwNiyH9w0wICvU3sL_YA';
+const engineId = '41332f9237c50459d';
+const query = 'search trends' // the search term
+
+async function searchterm() {
+    const url = `https://www.googleapis.com/customsearch/v1?q=${encodeURIComponent(query)}&key=${searchKey}&cx=${engineId}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data.items);
+
+        if (data.searchInformation) { //an estimated value but not exact vaule since it would breach Google's TOS
+            console.log(`Here is total search results for "${query}": ${data.searchInformation.totalResults}` );// ` ` symbol use for quotes
+        }
+        else {
+            console.log('No search results were found for this search term!');
+        }   
+    }
+    catch (error) {
+        console.error("No data searched", error);
+    }
+}
+
+searchterm();
+
+//<script async src="https://cse.google.com/cse.js?cx=41332f9237c50459d">
+//</script>
+//<div class="gcse-search"></div>
