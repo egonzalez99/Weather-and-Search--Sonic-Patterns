31c2f55716d7343cf18751a70cbde1a4c90b4002
egonzalez99 <112592570+egonzalez99@users.noreply.github.com>
2025-03-16 21:28:31 -0400

    made audio play

--- dataSonification.js
+++
@@ -1,7 +0,0 @@
-// creating audio from the web audio api
-
-// get the audio element
-const audioElement = document.querySelector("audio");
-
-// pass it into the audio context
-const track = audioContext.createMediaElementSource(audioElement);
